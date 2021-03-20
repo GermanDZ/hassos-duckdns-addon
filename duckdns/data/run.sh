db30ede695e15dac521b1c506b7207da636b2445
GermanDZ <germ@ndz.com.ar>
2021-03-20 15:03:00 +0100

    Add debug info

--- duckdns/data/run.sh
+++ duckdns/data/run.sh
@@ -56,6 +56,8 @@ while true; do
     [[ ${IPV4} != *:/* ]] && ipv4=${IPV4} || ipv4=$(curl -s -m 10 "${IPV4}")
     [[ ${IPV6} != *:/* ]] && ipv6=${IPV6} || ipv6=$(curl -s -m 10 "${IPV6}")
 
+    echo "Updating #{DOMAINS} to ${ipv4}"
+
     if answer="$(curl -s "https://www.duckdns.org/update?domains=${DOMAINS}&token=${TOKEN}&ip=${ipv4}&ipv6=${ipv6}&verbose=true")"; then
         bashio::log.info "${answer}"
     else
